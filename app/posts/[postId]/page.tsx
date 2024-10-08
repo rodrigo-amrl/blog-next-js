7368ccbd0c015805a0b37abda496bc26e30983dd
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-10-08 17:00:41 -0300

    set static params

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -3,6 +3,11 @@ import { getPostData, getSortedPostsData } from "@/lib/posts"
 import Link from "next/link";
 import { notFound } from "next/navigation"
 
+export function generateStaticParams() {
+    const posts = getSortedPostsData()
+    return posts.map((post) => ({ PostId: post.id }))
+
+}
 export function generateMetadata({ params }: { params: { postId: string } }) {
     const posts = getSortedPostsData()
     const { postId } = params
